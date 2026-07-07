f18ced7717df4fd88b42af5c54bc30aad07c6890
arsh-siddiqui <74855274+arsh-siddiqui@users.noreply.github.com>
2026-07-07 14:33:43 +0530

    fix: allow geolocation in Permissions-Policy

--- next.config.mjs
+++ next.config.mjs
@@ -24,7 +24,7 @@ const nextConfig = {
           },
           {
             key: "Permissions-Policy",
-            value: "camera=(), microphone=(), geolocation=()",
+            value: "camera=(), microphone=(), geolocation=(self)",
           },
         ],
       },
